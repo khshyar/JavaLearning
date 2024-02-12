4c92406ccc8ae93d23dbf2c9b3257cb3f06925ff
Khashayar Behnam <khshyarg@gmail.com>
2024-02-12 01:04:47 +0100

    DiscWAmir

--- src/pacman/Main.java
+++ src/pacman/Main.java
@@ -8,6 +8,7 @@ public class Main {
         ok = pacman.moveLeft();
         ok = pacman.moveTop();
         ok = pacman.moveRight();
+        ok = pacman.moveRight();
 
         System.out.println("Pac-Man X-Position: " + pacman.x);
         System.out.println("Pac-Man Y-Position: " + pacman.y);
